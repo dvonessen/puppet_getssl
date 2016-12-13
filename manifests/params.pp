72079558f72f2af7e5300b468c10d277518b8b3a
dthielking <github@thielking-vonessen.de>
2016-12-13 12:59:30 +0100

    renamed global variable withoud prefix use own default values for domain specific variables

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,28 +12,28 @@ class getssl::params{
   $production                = false
 
   # Configuration of global getssl config file
-  $global_account_mail       = undef
-  $global_account_key_length = 4096
-  $global_private_key_alg    = 'rsa'
-  $global_reload_command     = undef
-  $global_reuse_private_key  = true
-  $global_renew_allow        = 30
-  $global_server_type        = 'https'
-  $global_check_remote       = true
-  $global_ssl_conf           = '/usr/lib/ssl/openssl.cnf'
+  $account_mail       = undef
+  $account_key_length = 4096
+  $private_key_alg    = 'rsa'
+  $reload_command     = undef
+  $reuse_private_key  = true
+  $renew_allow        = 30
+  $server_type        = 'https'
+  $check_remote       = true
+  $ssl_conf           = '/usr/lib/ssl/openssl.cnf'
 
   # Configuration of domain specific config file
   $domain                    = undef
   $sub_domains               = []
   $acl                       = []
   $use_single_acl            = true
-  $domain_account_mail       = $global_account_mail
-  $domain_account_key_length = $global_account_key_length
-  $domain_private_key_alg    = $global_private_key_alg
-  $domain_reload_command     = $global_reload_command
-  $domain_renew_allow        = $global_renew_allow
-  $domain_server_type        = $global_server_type
-  $domain_check_remote       = $global_check_remote
+  $domain_account_mail       = undef
+  $domain_account_key_length = 4096
+  $domain_private_key_alg    = 'rsa'
+  $domain_reload_command     = undef
+  $domain_renew_allow        = 30
+  $domain_server_type        = 'https'
+  $domain_check_remote       = true
   $domain_cert_location      = undef
   $domain_key_location       = undef
   $ca_cert_location          = undef
